1e32160bce233b15c86aaa253e52144d3f1f05e7
reyginsusas <sreygin@gmail.com>
2026-08-08 20:39:13 +0800

    Change to relative URL

--- resources/js/api.js
+++ resources/js/api.js
@@ -1,5 +1,5 @@
 // const API_URL = "http://127.0.0.1:8000/api/tasks";
-const API_URL = "http://127.0.0.1/api/tasks";
+const API_URL = "/api/tasks";
 const token = localStorage.getItem("myToken");
 
 // FETCH/GET DATA FROM API
